860c359c9c7d1316539a290817fb6dbe93f84391
NK <guskova.ns@yandex.ru>
2023-05-01 00:59:30 +0400

    My diplom commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://51d10b7a-a4d8-4fd2-b420-8a67cb173cc1.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+    "firstName": "Кот",
+    "lastName": "Котовский",
+    "address": "ул Котовская 5",
+    "metroStation": 55,
+    "phone": "+89997776543",
+    "rentTime": 5,
+    "deliveryDate": "2023-05-01",
+    "comment": "",
+    "color": []
+}
